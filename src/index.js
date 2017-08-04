7c3e2956ebd52e1455b9ccc7c5734a50095f6eaa
loren.grixti <lorengrx@gmail.com>
2017-08-04 15:00:15 +0200

    Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Redux
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import {reducers} from './reducers/index';
 
 //App Components
 import Datacenter from './components/datacenter';
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(routerMiddleware(browserHistory))
+));
+
 ReactDOM.render(
-    <Datacenter/>,
+    <Provider store={store}>
+        <Datacenter/>
+    </Provider>,
     document.getElementById('datacenter_container')
 );
