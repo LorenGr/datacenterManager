10f41d83b8b724ab810c1c104cdf3f61376b90f9
loren.grixti <lorengrx@gmail.com>
2017-08-06 23:30:02 +0200

    refactor logic

--- src/reducers/datacenter.js
+++ src/reducers/datacenter.js
@@ -4,7 +4,10 @@ export default function datacenter(state = {
     servers: {}, apps: {}
 }, action) {
     let new_state = JSON.parse(JSON.stringify(state));
-    const generateID = () => new Date().getTime();
+    const generateID = () => new Date().getTime(),
+        findIndexAndRemove = (arr, callback) => {
+            arr.splice(arr.findIndex(callback), 1)
+        }
     let unique_id, sorted_servers;
 
     switch (action.type) {
@@ -20,42 +23,52 @@ export default function datacenter(state = {
         case "DESTROY_SERVER" :
             if (!Object.keys(new_state.servers).length) return new_state;
             sorted_servers = sortBy(new_state.servers, 'created').reverse();
+            let purgedAppInstances = new_state.servers[sorted_servers[0].id].apps;
             delete new_state.servers[sorted_servers[0].id];
+            if (purgedAppInstances.length) {
+                purgedAppInstances.forEach(instance => {
+                        findIndexAndRemove(
+                            new_state.apps[instance.application.id],
+                            (appInstance) => appInstance.id === instance.id
+                        );
+                    }
+                );
+            }
             return new_state;
 
         case "ADD_APP_INST" :
 
             if (!Object.keys(new_state.servers).length) return new_state;
             sorted_servers = sortBy(new_state.servers, 'created').reverse();
-            let available_server = false;
+            let availableServer = false;
 
             sorted_servers.map((server) => {
                 if (server.apps.length < 2) {
-                    available_server = server.id;
+                    availableServer = server.id;
                     return false;
                 }
             });
-            if (!available_server) return new_state;
+            if (!availableServer) return new_state;
 
             unique_id = generateID();
             let app = {
                 'created': unique_id,
                 'id': unique_id,
                 'application': action.app,
-                'server': available_server
+                'server': availableServer
             };
             if (!new_state.apps[action.app.id]) new_state.apps[action.app.id] = [];
             new_state.apps[action.app.id].push(app);
-            new_state.servers[available_server].apps.push(app);
+            new_state.servers[availableServer].apps.push(app);
             return new_state;
 
         case 'DESTROY_APP_INST' :
             if (!new_state.apps[action.app.id].length) return new_state;
-            let last_instance = new_state.apps[action.app.id].pop();
-            let purged_index = new_state.servers[last_instance.server].apps.findIndex(
-                (app) => app.id === last_instance.id
+            let lastInstance = new_state.apps[action.app.id].pop();
+            findIndexAndRemove(
+                new_state.servers[lastInstance.server].apps,
+                (app) => app.id === lastInstance.id
             );
-            new_state.servers[last_instance.server].apps.splice(purged_index, 1);
             return new_state;
 
         default :
